a9c9cc8fa50e2b7e6d33ede2d6feab8e9d06a71c
Sveta <svetasw_17@mail.ru>
2022-12-16 11:38:20 +1100

    43

---
+++ 43/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine($"Find the point of intersection of two lines");
+
+double[,] coeff = new double[2, 2];
+double[] crossPoint = new double[2];
+
+void InputCoefficients(){
+  for (int i = 0; i < coeff.GetLength(0); i++)
+  {
+    Console.Write($"Enter the coefficient {i+1} of the equation (y = k * x + b):\n");
+    for (int j = 0; j < coeff.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Enter coefficient k: ");
+      else Console.Write($"Enter coefficient b: ");
+      coeff[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] Decision(double[,] coeff)
+{
+  crossPoint[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
+  crossPoint[1] = crossPoint[0] * coeff[0,0] + coeff[0,1];
+  return crossPoint;
+}
+
+void OutputResponse(double[,] coeff)
+{
+  if (coeff[0,0] == coeff[1,0] && coeff[0,1] == coeff[1,1]) 
+  {
+    Console.Write($"\n Lines match");
+  }
+  else if (coeff[0,0] == coeff[1,0] && coeff[0,1] != coeff[1,1]) 
+  {
+    Console.Write($"\n Lines are parallel");
+  }
+  else 
+  {
+    Decision(coeff);
+    Console.Write($"\n Point of intersection of lines: ({crossPoint[0]}, {crossPoint[1]})");
+  }
+}
+
+InputCoefficients();
+OutputResponse(coeff);
