00dd3129772ae0144317d3bd187e1f29c205a421
Sveta <svetasw_17@mail.ru>
2022-12-16 11:37:52 +1100

    41

---
+++ 41/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine($"How many numbers greater than 0 the user entered \n");
+Console.Write($"Enter number М(number of numbers): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Enter {i+1} number: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Number greater than 0 entered: {Comparison(massiveNumbers)} ");
